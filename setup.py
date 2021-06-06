be303bee206258d7e185c9633b8ee19106c14038
George <teozz89@gmail.com>
2021-06-06 14:31:39 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'src',
+    description = 'utils for nlp in spacy 2.0',
+    packages = find_packages(),
+    author = 'tzz',
+    version = '0.1.0'
+)
